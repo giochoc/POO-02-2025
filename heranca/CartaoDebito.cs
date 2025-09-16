16aaefc328820d08bd0785d8e3b1301405febc3f
giochoc <giih.christina19@gmail.com>
2025-09-16 00:11:02 +0000

    feat: classe cartao

---
+++ heranca/CartaoDebito.cs
@@ -0,0 +1,25 @@
+using System.Dynamic;
+
+public class CartaoDebito
+{
+    public CartaoDebito()
+    {
+        Bandeira = BandeiraCartao.Visa;
+    }
+    public string Numero { get; set; }
+    // public string Bandeira { get; set; }
+    public BandeiraCartao Bandeira { get; set; }
+    public DateTime Vencimento { get; set; }
+    public string Portador { get; set; }
+    public string Cvv { get; set; }
+
+
+}
+
+public enum BandeiraCartao
+{
+    Visa,
+    Mastercard,
+    Amex,
+    Elo
+}
